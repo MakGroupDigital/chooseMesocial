8579d4676cd97acec8cd59679826ac4f4428ec1e
ekonzo <contact.makgroup.digital@gmail.com>
2026-05-23 16:27:48 +0100

    Fix production press media API routing

--- api/[...path].js
+++ api/[...path].js
@@ -1,3 +1,9 @@
 import app from '../choose-me web app/server.js';
 
-export default app;
+export default function handler(req, res) {
+  if (req.url && !req.url.startsWith('/api')) {
+    req.url = `/api${req.url.startsWith('/') ? req.url : `/${req.url}`}`;
+  }
+
+  return app(req, res);
+}
